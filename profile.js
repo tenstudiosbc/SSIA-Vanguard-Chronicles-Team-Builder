c79edaca028ee180d5e37e459b81234996df229c
TSBC <57209317+tenstudiosbc@users.noreply.github.com>
2025-09-08 18:39:57 +0700

    Create profile.js

---
+++ profile.js
@@ -0,0 +1,17 @@
+// profile.js
+const profiles = {
+  jones: {
+    portrait: "images/jones.png",
+    full: "images/jones_full.png"
+  },
+  kenziet: {
+    portrait: "images/kenziet.png"
+  },
+  hannabi: {
+    portrait: "images/hannabi.png"
+  },
+  elaria: {
+    portrait: "images/elaria.png"
+  }
+  // ...add others
+};
